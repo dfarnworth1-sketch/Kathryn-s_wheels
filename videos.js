ee3bb98fc5367efb9e539fc816966dfc63772b99
dfarnworth1-sketch <dfarnworth1@googlemail.com>
2026-08-05 22:40:04 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -362,7 +362,12 @@ const toolkitData = [
         title: "Wellingborough Crossroads",
         type: "image",
         url: "Wellingborough crossroads.jpg"
-   },
+ },
+           {
+    title: "📍 Open in Google Maps - Wellingborough Crossroads",
+    type: "link",
+    url: "https://maps.app.goo.gl/Wh7v6H6Pxn51Qmon7?g_st=ac"
+},
 {
     title: "Hardwater Crossing",
     type: "image",
