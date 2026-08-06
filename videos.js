5c2d7cc5c636d86fbc8c46c7f7714d56a0f4ffdc
dfarnworth1-sketch <dfarnworth1@googlemail.com>
2026-08-06 14:41:19 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -480,7 +480,7 @@ const toolkitData = [
         ]
       },
       {
-       name: "Standards Check",
+       name: "ADI Training videos",
         videos: [{
     title: "Standards Check - Live Training",
     id: "y5AFELcGlDI"
