a0975895a881695abb8c49123ec0150305480882
dfarnworth1-sketch <dfarnworth1@googlemail.com>
2026-08-08 08:27:46 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,81 +1,87 @@
+// Deployment refresh
 const toolkitData = [
   {
     name: "Vehicle Controls",
     icon: "🚗",
-    topics: [
+    topics:  [
       {
         name: "Cockpit Drill",
         videos: [
-          { title: "Cockpit Drill – Video 1", id: "06oNy6OPR-U" },
-          { title: "Cockpit Drill – Video 2", id: "VK2bEC1eqBc" }
+          { title: "Ultimate Guide to Cockpit Drill", id: "06oNy6OPR-U" },
+          { title: "Tips For Best Seating Postion", id: "VK2bEC1eqBc" }
         ]
       },
       {
         name: "Clutch",
         videos: [
           { title: "Manual Clutch Working", id: "lqo0_StXf4M" },
+          {title: "Understanding the Clutch", id: "devo3kdSPQY"},
           { title: "Clutch Operation", id: "ErCw1FfLPf4" },
-          { title: "Clutch Demonstration", id: "k1SBFfMbksI" },
-          { title: "Clutch – Video 4", id: "kLGpfdRFxxU" },
-          { title: "Clutch – Video 5", id: "09DJbS1ntkQ" },
-          { title: "Clutch – Video 6", id: "AHna4amcsnE" },
-          { title: "Clutch – Video 7", id: "khvrc5NOQGo" }
+          { title: "Clutch Control - Different Techniques with Advantages and Disadvantages", id: "k1SBFfMbksI" },
+          { title: "When To Press The Clutch Down In A Manual Car", id: "kLGpfdRFxxU" },
+          { title: "Clutch Down Jerk? How To Make It Smooth", id: "09DJbS1ntkQ" },
+          { title: "How Long Should You Hold The Clutch At The Bite Point In A Manual Car", id: "AHna4amcsnE" },
+          { title: "Important For Mastering Clutch Control - Heel Position", id: "khvrc5NOQGo" }
         ]
       },
       {
         name: "Gears",
-        videos: [
+        videos: [{ title: "Ultimate Guide to Gears", id: "w8RfEyt7Stk"},
+                 { title: "Gear and Steer", id: "mOr5Gqx6ZoM" },
+                 { title: "Why Does My Car Jerk When Changing Gear?", id: "Ifn0OI3v7lc" },
           { title: "Using the Gears", id: "A4rs09AKBc8" },
-          { title: "Gears – Video 2", id: "oM25BCDVHSw" },
-          { title: "Gears – Video 3", id: "Ifn0OI3v7lc" },
-          { title: "Gears – Video 4", id: "mOr5Gqx6ZoM" }
+          { title: "Tips For Smoother Gear Changing", id: "oM25BCDVHSw" }
+          
+          
         ]
       },
       {
         name: "Steering",
-        videos: [
-          { title: "Steering", id: "R8zhtcN_4u4" }
+        videos: [{
+    title: "Ultimate Guide to Steering",
+    id: "VxtyM11NJlc"
+},
+          { title: "How to Steer", id: "R8zhtcN_4u4" }
         ]
       },
       {
+        name: "Signals",
+        videos: [{
+  title: "Ultimate Guide to Signals",
+  id: "fIuVFr6FuSQ"
+}
+        ]
+      },
+      { 
         name: "Brakes",
         videos: [
-         {
+          {
   title: "Progressive Braking",
   type: "image",
   url: "Braking.jpg"
-}, 
-          { title: "Using the Brakes", id: "Jl9qTbZH4tM" }
+},
+          { title: "Clutch or Brake First", id: "Jl9qTbZH4tM" }
         ]
       },
-      {
-        name: "Positioning",
-        videos: [
-          { title: "Road Positioning", id: "dR1ccDljtmM" },
-          {
-            title: "Judging Clearance from Parked Cars",
-            id: "tIo8Cv7lY0c"
-          },
-          { title: "Positioning – Video 3", id: "E_YE2NbjAFU" }
-        ]
-      }
-    ]
-  },
-
-  {
-    name: "Basic Driving",
+     ]
+     }, 
+     {
+    name: "Moving Off and Stopping",
     icon: "🚙",
     topics: [
       {
         name: "Moving Off and Stopping",
-        videos: [
+        videos: [{
+    title: "Ultimate Guide to Moving Off and Stopping",
+    id: "D1Gt_X-oyU0"
+},
           { title: "Moving Off and Stopping", id: "D1Gt_X-oyU0" },
           {
             title: "Avoiding and Recovering from a Stall",
             id: "dze2UWGT11k"
           },
           {
-            title: "Moving Off and Stopping – Video 3",
+            title: "Stoppin Close",
             id: "VNYlSPYJTjM"
           }
         ]
@@ -86,27 +92,17 @@ const toolkitData = [
           { title: "Mirrors and the MSM Routine", id: "esPeCo8aT7s" },
           { title: "Adjusting the Mirrors", id: "MI_p031-q60" },
           { title: "Using Mirrors When Reversing", id: "w7m0DuL6REw" },
-          { title: "Mirrors – Video 4", id: "obwdyqW1ars" }
-        ]
-      },
-      {
-        name: "Signals",
-        videos: []
-      },
-      {
-        name: "Observation",
-        videos: [
-          { title: "Blind Spots", id: "2cr5A_R87iU" }
+          { title: "Mirrors", id: "obwdyqW1ars" }
         ]
       },
       {
         name: "Observation and Planning",
         videos: [
+           { title: "Blind Spots", id: "2cr5A_R87iU" },
+          { title: "Selective Attention Test", id: "vJG698U2Mvo" },
           { title: "Observation and Planning", id: "DJcAgnBFRds" },
-          {
-    title: "How to Deal with Cyclists",
-    id: "UPN_XvaYU2k"
-}
+    { title: "How to Deal with Cyclists",
+    id: "UPN_XvaYU2k"}
         ]
       }
     ]
@@ -118,22 +114,30 @@ const toolkitData = [
     topics: [
       {
         name: "Junctions",
-        videos: [
+        videos: [ { title: "Junctions – Ultimate Guide To Junctions", id: "JFs5yEushSg" },
           { title: "Junctions and Turns Made Easy", id: "EJVxCsRiyOw" },
           { title: "Junctions – Video 2", id: "HkCyv2_ch6M" },
           { title: "Junctions – Video 3", id: "MXUn-EYPpkA" },
           { title: "Junctions – Video 4", id: "Q8llvZla94Y" },
           { title: "Junctions – Video 5", id: "OjM5yzCFDYQ" },
           { title: "Junctions – Video 6", id: "GUocIPsXbcQ" },
-          { title: "Junctions – Video 7", id: "JFs5yEushSg" },
+         
           { title: "Roundabouts and Junctions", id: "IqNW0IrOjzE" }
         ]
       },
       {
-        name: "Positioning at Junctions",
+        name: "Positioning",
         videos: [
-          { title: "Positioning at Junctions", id: "WvDiRRjKWdw" }
-        ]
+          { title: "Positioning at Junctions", id: "WvDiRRjKWdw" },
+        
+        { title: "Road Positioning", id: "dR1ccDljtmM" },
+          {
+            title: "Judging Clearance from Parked Cars",
+            id: "tIo8Cv7lY0c"
+          },
+          { title: "Positioning – Video 3", id: "E_YE2NbjAFU" }
+      
+    ]
       }
     ]
   },
@@ -144,19 +148,26 @@ const toolkitData = [
     topics: [
       {
         name: "Roundabouts",
-        videos: [
-          { title: "Roundabouts Driving Lesson", id: "koK_a4KYs-E" },
-          {
-            title: "Double Mini and Magic Roundabouts",
-            id: "Bup0xALjAVs"
-          },
-          {
-            title: "Understanding Roundabout Signs",
-            id: "yYWlP4RPFEY"
-          },
-          { title: "Roundabouts – Video 4", id: "6WgewaIleag" },
-          { title: "Roundabouts – Video 5", id: "nAyVtw8Agu0" },
-          { title: "Roundabouts – Video 6", id: "1vmAw74Eags" }
+       videos: [{
+    title: "Ultimate Guide to Roundabouts",
+    id: "1AHBhWsfAmI"
+},
+    {
+        title: "Ultimate Guide to Multi Lane Roundabouts",
+        id: "A2rrrcM4GH4"
+    },
+    { title: "Roundabouts", id: "koK_a4KYs-E" },
+    {
+        title: "Double Mini and Magic Roundabouts",
+        id: "Bup0xALjAVs"
+    },
+    {
+        title: "Did you Know This About Road Signs",
+        id: "yYWlP4RPFEY"
+    },
+    { title: "Spiral Roundabouts - Which Lane", id: "6WgewaIleag" },
+    { title: "Which Exit Lane", id: "nAyVtw8Agu0" },
+    { title: "Spiral Roundabouts", id: "1vmAw74Eags" }
         ]
       }
     ]
@@ -184,15 +195,25 @@ const toolkitData = [
       {
         name: "Dual Carriageways",
         videos: []
-      },
-      {
-        name: "Motorways",
-        videos: []
       }
     ]
   },
 
   {
+    name: "Traffic Lights",
+    icon: "🚦",
+    topics: [
+        {
+            name: "Traffic Lights",
+            videos: [{
+    title: "Ultimate Guide to Traffic Lights",
+    id: "dJcciOp_TEQ"
+},
+            ]
+        }
+    ]
+}, 
+   {
     name: "Manoeuvres",
     icon: "🅿️",
     topics: [
@@ -236,65 +257,36 @@ const toolkitData = [
   },
 
   {
-    name: "Test Preparation",
-    icon: "📋",
+    
+    name: "Emergency Stop",
+    icon: "🚨",
     topics: [
       {
-        name: "Show Me, Tell Me",
+        name: "Emergency Stop",
         videos: [
-      {
-        title: "Show Me, Tell Me" ,
-        id: "uh8slnP76-w"
-          }
+          { title: "Emergency Stop", id: "c_F8VZwWw38" }
         ]
-      },
-     { 
-        name: "Independent Driving",
-        videos: [
-          {
-            title: "Using a Sat Nav on the Driving Test",
-            id: "SaapXrhuEp8"
-          }
-        ]
-      },
-      {
-        name: "Mock Test",
-        videos: [
-    {
-        title: "GOV.UK Ready to Pass - Mock Tests",
-        type: "link",
-        url: "https://readytopass.campaign.gov.uk/mock-driving-tests/"
-      },
-{
-    title: "DVSA - Carry Out Mock Driving Tests",
-    type: "link",
-    url: "https://www.gov.uk/guidance/carry-out-mock-driving-tests-for-your-pupils"
-
-    }
-]
-      },
-      {
-        name: "Driving Test",
-        videos: [
-          {
-            title: "How Many Lessons Are Needed to Pass?",
-            id: "YwgzdI03nJk"
-           }
-        ]
-      },
-      {
-  name: "Kettering – Tricky Areas",
+     
+      }
+    ]
+  },
+ {
+    name: "Tricky Areas",
+    icon: "📍",
+    topics: [
+        {
+            name: "Kettering – Tricky Areas",
   videos: [{
   title: "Double Roundabout ",
   type: "image",
   url: "Double RB 1.jpg"
 },
 {
-    title: "View on Google Maps",
+    title: "📍 Open in Google Maps",
     type: "link",
     url: "https://maps.app.goo.gl/XW14MzfB24HoAaiX7?g_st=ac"
 },
-{
+  {
   title: "Double Roundabout (3 lanes on approach)",
   type: "image",
   url: "Double RB 2 (3 lanes on approach).jpg"
@@ -323,18 +315,18 @@ const toolkitData = [
     title: "Broughton",
     type: "image",
     url: "Broughton.jpg"
-},
-           {
+  },
+         {
     title: "📍 Open in Google Maps",
     type: "link",
     url: "https://maps.app.goo.gl/kEVR1kQT5GPNn9wP7?g_st=ac"
-},
-  {
+},  
+{
     title: "A14 - J7 Roundabout",
     type: "image",
     url: "A14 - J7 Roundabout.jpg"
- },
-          {
+  },
+           {
     title: "📍 Open in Google Maps - A14 J7 Roundabout",
     type: "link",
     url: "https://maps.app.goo.gl/9wKQ6J58o76nJYnm7?g_st=ac"
@@ -357,7 +349,7 @@ const toolkitData = [
         title: "Wellingborough Crossroads",
         type: "image",
         url: "Wellingborough crossroads.jpg"
- },
+},
            {
     title: "📍 Open in Google Maps - Wellingborough Crossroads",
     type: "link",
@@ -367,166 +359,74 @@ const toolkitData = [
     title: "Hardwater Crossing",
     type: "image",
     url: "Hardwater crossing.jpg"
-   },
-{
-    title: "Hardwater Crossing",
-    type: "image",
-    url: "Hardwater crossing.jpg"
     },
            {
     title: "📍 Open in Google Maps - Hardwater Crossing",
     type: "link",
     url: "https://maps.app.goo.gl/EktaTJzKA4f2uc7P9?g_st=ac"
-   }
-]
-      },
-    ]  
-  },
-    {
-    name: "Emergency Situations",
-    icon: "🚨",
-    topics: [
-      {
-        name: "Emergency Stop",
-        videos: [
-          { title: "Emergency Stop", id: "c_F8VZwWw38" }
-        ]
-      },
-      {
-        name: "Stopping on a High-Speed Road",
-        videos: [
-          {
-            title: "Stopping on a High-Speed Road",
-            id: "nc5ZiazjEEY"
-          }
-        ]
       }
-    ]
-  },
-
+      ]
+}
+]
+},       
   {
-    name: "Instructor Hub",
+    name: "Mindset and Test Nerves",
     icon: "👨‍🏫",
     topics: [
            {
-        name: "Daily Lesson Framework",
-        videos: [
-          {
-            title: "ADI Framework",
-            type: "pdf",
-            url: "ADI_Framework_Final.pdf"
-          },
-          {
-            title: "Commander's Intent Framework",
-            type: "pdf",
-            url: "Commanders_Intent_Framework_Final.pdf"
-          }
-        ]
-      },
-      {
-        name: "First Lesson Assessment",
-        videos: []
-      },
-      {
-        name: "Risk Management",
-        videos: []
-      },
-      {
-        name: "Reflection",
-        videos: []
-      },
-      {
         name: "Thoughts and Feelings",
         videos: [
           { title: "Anxiety – Video 1", id: "wiNdXiZzPPA" },
           { title: "Anxiety – Video 2", id: "0yKXFrpGAM8" },
           { title: "Anxiety – Video 3", id: "IoLPvEc-0IQ" },
           { title: "Anxiety – Video 4", id: "kFxO0fEJLmA" }
-        ]
-      },
-      {
-        name: "Neurodiversity",
-        videos: [
-          { title: "ADHD", id: "aLWFvLZbJEI" }
-        ]
-      },
-      {
-        name: "Learning and Coaching",
-        videos: [
-          {
-            title: "A Logical Order for Learning to Drive",
-            id: "FMj9SvIGfcg"
-          },
-          { title: "Selective Attention Test", id: "vJG698U2Mvo" }
-        ]
-      },
-      {
-        name: "GDE Matrix",
-        videos: [
-          {
-            title: "Goals for Driver Education",
-            id: "Qck7EyKOFjw"
-    },
-    {
-      title: "GDE Matrix PDF",
-      type: "pdf",
-      url: "GDE-matrix.pdf"
-    }
-  ]
-},
-  {
-        name: "Advanced Driving",
-        videos: [
-          {
-            title: "How to Pass an Advanced Driving Course",
-            id: "7ZD3NZ4H6K8"
-          }
-        ]
-      },
-      {
-       name: "ADI Training videos",
-        videos: [{
-    title: "Standards Check - Live Training",
-    id: "y5AFELcGlDI"
- },
-      {
-    title: "Skills or topics",
-    id: "BODq8IDWrnE"
-      },
-      {
-    title: "Use T-GROW",
-    id: "uvqMWvOm2nk"
-},
-     { 
-    title: "Andrew Love, Chatting with ADIs",
-    id: "rxP1L2ft0NU"
-}, 
- {                 
-    title: "The Skill Every Coach Needs",
-    id: "8i31-c2zh6A"
- }
-  ]
-},
-      {
-        name: "The 17 Competencies",
-        videos: [
-        {
-      title: "Part 3 Marking Sheet",
-      type: "pdf",
-      url: "part-3-marking-sheet.pdf"
-    }
-  ]
-},
-      {
-        name: "DT1 Notes",
-        videos: [
-          {
-            title: "DVSA DL25 Driving Test Report",
-            type: "pdf",
-            url: "dl25-driving-test-report.pdf"
-          }
+       
         ]
       }
     ]
-  }
+  },
+ { 
+     name: "Driving Test",
+    icon: "📋",
+    topics: [
+      {
+        name: "Show Me, Tell Me",
+        videos: [
+      {
+        title: "Show Me, Tell Me" ,
+        id: "uh8slnP76-w"
+          }
+        ]
+      },
+     { 
+        name: "Independent Driving",
+        videos: [
+          {
+            title: "Using a Sat Nav on the Driving Test",
+            id: "SaapXrhuEp8"
+   }
+        ]
+      }
+    ]
+  },
+ {
+  name: "GDE Matrix",
+  icon: "📋",
+  topics: [
+    {
+      name: "GDE Matrix",
+      videos: [
+        {
+          title: "GDE Matrix",
+          type: "image",
+          url: "GDE Matrix.jpg"
+        },
+        {
+          title: "GDE Matrix Explained",
+          id: "Qck7EyKOFjw"
+    }
+  ]
+}
+  ]
+ }     
 ];
